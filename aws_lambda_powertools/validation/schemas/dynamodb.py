bde72f73d1f0c12170c148d20168a365219e5b4c
Ran Isenberg <ran.isenberg@cyberk.com>
2020-08-16 23:02:11 +0300

    feat: RFC: Validate incoming and outgoing events utility #95

---
+++ aws_lambda_powertools/validation/schemas/dynamodb.py
@@ -0,0 +1,37 @@
+from datetime import date
+from typing import Any, Dict, List, Optional
+
+from pydantic import BaseModel, root_validator
+from typing_extensions import Literal
+
+
+class DynamoScheme(BaseModel):
+    ApproximateCreationDateTime: date
+    Keys: Dict[Literal["id"], Dict[Literal["S"], str]]
+    NewImage: Optional[Dict[str, Any]] = {}
+    OldImage: Optional[Dict[str, Any]] = {}
+    SequenceNumber: str
+    SizeBytes: int
+    StreamViewType: Literal["NEW_AND_OLD_IMAGES", "KEYS_ONLY", "NEW_IMAGE", "OLD_IMAGE"]
+
+    @root_validator
+    def check_one_image_exists(cls, values):
+        newimg, oldimg = values.get("NewImage"), values.get("OldImage")
+        stream_type = values.get("StreamViewType")
+        if stream_type == "NEW_AND_OLD_IMAGES" and not newimg and not oldimg:
+            raise TypeError("DynamoDB streams schema failed validation, missing both new & old stream images")
+        return values
+
+
+class DynamoRecordSchema(BaseModel):
+    eventID: str
+    eventName: Literal["INSERT", "MODIFY", "REMOVE"]
+    eventVersion: float
+    eventSource: Literal["aws:dynamodb"]
+    awsRegion: str
+    eventSourceARN: str
+    dynamodb: DynamoScheme
+
+
+class DynamoDBSchema(BaseModel):
+    Records: List[DynamoRecordSchema]
